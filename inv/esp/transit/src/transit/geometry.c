9e7c689155934936ac1726e8dd9c3248f8516769
pato <pato@202cf5a2-c0b7-428c-b106-5c802e8d4b69>
2004-05-10 03:25:14 +0000

    Initialize geometry

--- inv/esp/transit/src/transit/geometry.c
+++ inv/esp/transit/src/transit/geometry.c
@@ -32,26 +32,40 @@ int
 setgeom(struct transit *tr)
 {
   static struct geometry st_sg;
+  memset(&st_sg,0,sizeof(struct geometry));
+
   struct geometry *sg=tr->ds.sg=&st_sg;
   struct geometry *hg=&tr->ds.th->sg;
 
-  sg->smaxis=hg->smaxis;
-
-  //If values are not hinted, then use hard coded values: 
-  sg->smaxis=hg->smaxis>0?hg->smaxis:1;
-  sg->time=hg->time>0?hg->time:0;
-  sg->starmass=hg->starmass>0?hg->starmass:1;
-  sg->starrad=hg->starrad>0?hg->starrad:1;
 
   //If factor values are correctly hinted then use them, otherwise
   //assume hard coded values. If you change the following lines, don't
   //forget to change the help page in transit.h that the semimajor axis
   //is in AU; timing is in hours from mid eclipse; star's mass and
   //radius are in solar units.
-  sg->smaxisfct=hg->smaxisfct>0?hg->smaxisfct:AU;
-  sg->timefct=hg->timefct>0?hg->timefct:HOUR;
-  sg->starmassfct=hg->starmassfct>0?hg->starmassfct:SUNMASS;
-  sg->starradfct=hg->starradfct>0?hg->starradfct:SUNRADIUS;
+  sg->smaxisfct  =hg->smaxisfct>0  ?hg->smaxisfct  :AU;
+  sg->timefct    =hg->timefct>0    ?hg->timefct    :HOUR;
+  sg->eccfct     =hg->eccfct>0     ?hg->eccfct     :1;
+  sg->inclfct    =hg->inclfct>0    ?hg->inclfct    :DEGREES;
+  sg->aperfct    =hg->aperfct>0    ?hg->aperfct    :DEGREES;
+  sg->lnode      =hg->lnodefct>0   ?hg->lnodefct   :DEGREES;
 
+  sg->starmassfct=hg->starmassfct>0?hg->starmassfct:SUNMASS;
+  sg->starradfct =hg->starradfct>0 ?hg->starradfct :SUNRADIUS;
+
+
+  //If values are not hinted, then use hard coded values: 
+  sg->smaxis  =hg->smaxis>0  ?hg->smaxis  :AU/sg->smaxisfct;
+  sg->time    =hg->time>0    ?hg->time    :0;
+  sg->ecc     =hg->ecc>0     ?hg->ecc     :0;
+  sg->incl    =hg->incl>0    ?hg->incl    :0;
+  sg->aper    =hg->aper>0    ?hg->aper    :0;
+  sg->lnode   =hg->lnode>0   ?hg->lnode   :0;
+
+  sg->starmass=hg->starmass>0?hg->starmass:SUNMASS/sg->starmass;
+  sg->starrad =hg->starrad>0 ?hg->starrad :SUNRADIUS/sg->starradfct;
+
+
+  tr->pi|=TRPI_GEOMETRY;
   return 0;
 }
