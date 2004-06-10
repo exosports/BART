7d077c26e7cf96d2c1ad8989b3b5cb8ce2b489e8
pato <pato@202cf5a2-c0b7-428c-b106-5c802e8d4b69>
2004-06-10 17:31:16 +0000

    Added check for cproto, corrected some typos

--- inv/esp/transit/include/proto_argum.h
+++ inv/esp/transit/include/proto_argum.h
@@ -8,6 +8,7 @@
 extern int processparameters P_((int argc, char **argv, struct transit *tr));
 extern int acceptsoltype P_((transit_ray_solution **sol, char *hname));
 extern int acceptgenhints P_((struct transit *tr));
-extern void savehint P_((FILE *out, struct transithint *hint));
+extern void savehint P_((FILE *out, struct transithint *hints));
+extern int resthint P_((FILE *in, struct transithint **hint));
 
 #undef P_
