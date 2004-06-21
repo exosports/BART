1e9b23d33e3e9c05437ee1045e70c54ede6a45a8
pato <pato@202cf5a2-c0b7-428c-b106-5c802e8d4b69>
2004-06-21 19:47:44 +0000

    No optimizacion while debugging

--- inv/esp/transit/include/proto_sampling.h
+++ inv/esp/transit/include/proto_sampling.h
@@ -10,6 +10,6 @@ extern int lineinterpol P_((int ndat, double *x, double *y, int n, long *indx, f
 extern void natcubspline P_((int ndat, double *x, double *y, int n, long *indx, float *t, double *yout, double *dout));
 extern inline void natcubsplinecoef P_((long n, double *x, double *y, double *h, double *D));
 extern inline double interp P_((double refx, double *x, double *y, long n, int intkind));
-extern double lineinterp P_((double refx, double *x, double *y, long n));
+extern inline double lineinterp P_((double refx, double *x, double *y, long n));
 
 #undef P_
