d2454e1599fdb91980e5cfb99c92ad510c5f51a8
pato <pato@202cf5a2-c0b7-428c-b106-5c802e8d4b69>
2004-09-17 20:05:23 +0000

    Support for data output while debugging

--- inv/esp/transit/include/proto_extinction.h
+++ inv/esp/transit/include/proto_extinction.h
@@ -7,6 +7,7 @@
 /* extinction.c */
 extern inline int newprofile P_((float **pr, int vf, double dwn, float dop, float lor, float ta));
 extern inline int extradius P_((long r, double rad, double **kiso, double temp, int fbinvoigt, float timesalpha, float maxratio));
+extern void outputinfo P_((char *outfile, long w, long dw, long ln, long dln, double **kiso, double timesalpha, int fbinvoigt, double temp, double rad));
 extern inline int computeextradius P_((long r, double rad, double temp, struct extinction *ex));
 extern int extwn P_((struct transit *tr));
 extern void printone P_((struct transit *tr));
