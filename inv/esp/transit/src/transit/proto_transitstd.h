388fe462ec932cf312a5307150854097b6db32c5
pato <pato@202cf5a2-c0b7-428c-b106-5c802e8d4b69>
2005-08-09 05:12:30 +0000

    deleting obsolete code. new verbfileopen

--- inv/esp/transit/src/transit/proto_transitstd.h
+++ inv/esp/transit/src/transit/proto_transitstd.h
@@ -9,7 +9,7 @@ extern inline void transitdot P_((int thislevel, int verblevel, ...));
 extern int transiterror P_((int flags, const char *str, ...));
 extern int vtransiterror P_((int flags, const char *str, va_list ap));
 extern int fileexistopen P_((char *in, FILE **fp));
-extern int verbfileopen P_((char *in, FILE **fp, char *desc));
+extern FILE *verbfileopen P_((char *in, char *desc));
 extern void transitcheckcalled P_((const long pi, const char *fcn, const int n, ...));
 extern void error P_((int exitstatus, int something, const char *fmt, ...));
 extern void free_isov P_((prop_isov *isov));
@@ -20,5 +20,6 @@ extern void free_samp P_((prop_samp *samp));
 extern void free_atm P_((prop_atm *atm));
 extern void savestr P_((FILE *out, char *str));
 extern int reststr P_((FILE *in, char **str));
+extern void linetoolong P_((int max, char *file, int line));
 
 #undef P_
