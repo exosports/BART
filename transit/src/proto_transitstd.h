1debd254706d6344bd8d573f9ae51a20e29f8ed0
pcubillos <pcubillos@knights.ucf.edu>
2013-12-02 06:11:54 -0500

    Added time-stamp prints in the main file for developing.

--- transit/src/proto_transitstd.h
+++ transit/src/proto_transitstd.h
@@ -21,6 +21,6 @@ extern void free_atm P_((prop_atm *atm));
 extern void savestr P_((FILE *out, char *str));
 extern int reststr P_((FILE *in, char **str));
 extern void linetoolong P_((int max, char *file, int line));
-extern void timecheck P_((int verblevel, long iter, long index, char *str, struct timeval tv1, struct timeval tv2));
+extern double timecheck P_((int verblevel, long iter, long index, char *str, struct timeval tv1, double t0));
 
 #undef P_
--- transit/src/transit.c
+++ transit/src/transit.c
@@ -32,16 +32,17 @@ int main(int argc,      /* Number of variables */
      assigned because the array that is going to point has not been
      initialized yet.  */
   struct transit transit;
-  struct timeval tv1, tv2;
   long itr=0;
+  struct timeval tv;
+  double t0=0.0;
 
   memset(&transit, 0, sizeof(struct transit));
   verblevel=2;
-  gettimeofday(&tv1, NULL);
+  //gettimeofday(&tv, NULL);
 
   /* Command line parameters' processing:        */
   fw(processparameters, !=0, argc, argv, &transit);
-  timecheck(verblevel, 0, 0, "processparameters", tv1, tv2);
+  t0 = timecheck(verblevel, 0, 0, "processparameters", tv, t0);
 
   /* Accept all general hints:                   */
   fw(acceptgenhints, !=0, &transit);
@@ -67,18 +68,18 @@ int main(int argc,      /* Number of variables */
   /* EDIT: The loop should enclose getatm (getatm will change in the future,
      but for the moment we will leave it as it is).  */
   for (itr=0; itr<2; itr++){
-    //timecheck(verblevel, itr, 0, "Start loop", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 0, "Start loop", tv, t0);
     /* Read line info:                             */
     fw(readlineinfo, !=0, &transit);
-    //timecheck(verblevel, itr, 1, "readlineinfo", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 1, "readlineinfo", tv, t0);
 
     /* Read Atmosphere information:                */
     fw(getatm, !=0, &transit);
-    //timecheck(verblevel, itr, 2, "getatm", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 2, "getatm", tv, t0);
 
     /* Make wavelength binning:                    */
     fw(makewavsample, <0, &transit);
-    //timecheck(verblevel, itr, 3, "makewavsample", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 3, "makewavsample", tv, t0);
     if(fw_status>0)
       transitprint(7, verblevel,
                    "makewavsample() modified some of the hinted "
@@ -87,7 +88,7 @@ int main(int argc,      /* Number of variables */
  
     /* Make wavenumber binning:                    */
     fw(makewnsample, <0, &transit);
-    //timecheck(verblevel, itr, 4, "makewnsample", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 4, "makewnsample", tv, t0);
     if(fw_status>0)
       transitprint(7, verblevel,
                    "makewnsample() modified some of the hinted "
@@ -96,7 +97,7 @@ int main(int argc,      /* Number of variables */
  
     /* Make radius binning and interpolate data to new value: */
     fw(makeradsample, <0, &transit);
-    //timecheck(verblevel, itr, 5, "makeradsample", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 5, "makeradsample", tv, t0);
     if(fw_status>0)
       transitprint(7, verblevel,
                    "makeradsample() modified some of the hinted "
@@ -105,19 +106,19 @@ int main(int argc,      /* Number of variables */
  
     /* Initializes CIA:                       */
     fw(interpolatecia, !=0, &transit);
-    //timecheck(verblevel, itr, 6, "interpolatecia", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 6, "interpolatecia", tv, t0);
  
     /* Computes index of refraction:          */
     fw(idxrefrac, !=0, &transit);
-    //timecheck(verblevel, itr, 7, "idxrefrac", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 7, "idxrefrac", tv, t0);
  
     /* Calculates extinction coefficient:     */
     fw(extwn, !=0, &transit);
-    //timecheck(verblevel, itr, 8, "extwn", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 8, "extwn", tv, t0);
  
     /* Computes sampling of impact parameter: */
     fw(makeipsample, <0, &transit);
-    //timecheck(verblevel, itr, 9, "makeipsample", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 9, "makeipsample", tv, t0);
     if(fw_status>0)
       transitprint(7, verblevel,
                    "makeipsample() modified some of the hinted "
@@ -126,30 +127,28 @@ int main(int argc,      /* Number of variables */
  
     /* Prints sampling info:                  */
     fw(outsample, !=0, &transit);
-    //timecheck(verblevel, itr, 10, "outsample", tv1, tv2);
- 
+    t0 = timecheck(verblevel, itr, 10, "outsample", tv, t0);
+
     /* Calculates optical depth:              */
     fw(tau, !=0, &transit);
-    //timecheck(verblevel, itr, 11, "tau", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 11, "tau", tv, t0);
 
-    transitprint(1, verblevel, "len: %i, word: %s.\n",
-                               (int)strlen(transit.f_out), transit.f_out); 
-   
-
+    //transitprint(1, verblevel, "len: %i, word: %s.\n",
+    //                           (int)strlen(transit.f_out), transit.f_out); 
     sprintf(str_iter, "%li", itr);
     strncpy(fout+dot_pos, str_iter, 1);
     strcpy(transit.f_out, fout);
 
     /* Calculates eclipse modulation:         */
     fw(modulation, !=0, &transit);
-    //timecheck(verblevel, itr, 12, "modulation", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 12, "modulation", tv, t0);
  
     free(transit.save.ext);
     freemem_isotopes (transit.ds.iso, &transit.pi);
     freemem_cia      (transit.ds.cia, &transit.pi);
     freemem_outputray(transit.ds.out, &transit.pi);
     transitprint(1, verblevel, "----------------------------\n");
-    timecheck(verblevel, itr, 13, "THE END", tv1, tv2);
+    t0 = timecheck(verblevel, itr, 13, "THE END", tv, t0);
   }
   freemem_transit(&transit);
 
