1c686907a2a3623c99fa2e09a7c7c2779f7ca358
Joseph Harrington <jh@physics.ucf.edu>
2014-04-11 16:05:06 -0400

    Wipe transit out of the repo, to prepare for pulling Patricio's repo as a submodule.

--- pu/scripts/magicadd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cat /etc/magic |grep -v 'TLI(Transit' >/tmp/magic.tmp
-
-cat >>/tmp/magic.tmp <./scripts/newmagic 
-
-cat /tmp/magic.tmp >/etc/magic
--- transit/src/proto_slantpath.h
+++
@@ -1,9 +0,0 @@
-#if __STDC__ || defined(__cplusplus)
-#define P_(s) s
-#else
-#define P_(s) ()
-#endif
-
-/* src/slantpath.c */
-
-#undef P_
