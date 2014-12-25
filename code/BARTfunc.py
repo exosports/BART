Fixed bug in PT calculation (needed to reverse order of profiles). Added readtransit.py funciton to read transit outputs.  Fixed bug in wine.py's kurucz spectrum (changed units from F_lambda to F_wavenumber).  Updated transit to fix memory leaks and take CIA read out of the loop.

code/BARTfunc.py
@@ -167,6 +167,8 @@ def main(comm):
   mu.msg(verb, "ICON FLAG 50")
   # Read atmospheric file to get data arrays:
   species, pressure, temp, abundances = mat.readatm(atmfile)
+  # Reverse pressure order (for PT to work):
+  pressure = pressure[::-1]
   nlayers  = len(pressure)   # Number of atmospheric layers
   nspecies = len(species)    # Number of species in the atmosphere
   mu.msg(verb, "There are {:d} layers and {:d} species.".format(nlayers,
@@ -299,20 +301,22 @@ def main(comm):
 
     # Input converter calculate the profiles:
     try:
-      profiles[0] = pt.PT_generator(pressure, params[0:nPT], MadhuPT)
+      profiles[0] = pt.PT_generator(pressure, params[0:nPT], MadhuPT)[::-1]
     except ValueError:
       mu.msg(verb, 'Input parameters give non-physical profile.')
       # FINDME: what to do here?
 
+    mu.msg(verb-10, "Temperature profile: {}".format(profiles[0]))
     # Scale abundance profiles:
     for i in np.arange(nmolfit):
       m = imol[i]
       # Use variable as the log10:
-      profiles[m+1] = (abundances[:, m] * 10.0**params[nPT+i])
+      profiles[m+1] = abundances[:, m] * 10.0**params[nPT+i]
     # Update H2, He abundances so sum(abundances) = 1.0 in each layer:
     q = 1.0 - np.sum(profiles[imetals+1], axis=0)
     profiles[iH2+1] = ratio * q / (1.0 + ratio)
     profiles[iHe+1] =         q / (1.0 + ratio)
+    #print("qH2O: {}  Qmetals: {}  QH2: {}  p: {}".format(params[nPT], q[50], profiles[iH2+1,50], profiles[:,50]))
 
     # transit calculates the model spectrum:
     mu.comm_scatter(transitcomm, profiles.flatten(), MPI.DOUBLE)

code/wine.py
@@ -137,7 +137,7 @@ def readkurucz(kfile, temperature, logg):
   Returns:
   --------
   starfl: 1D ndarray
-     Kurucz stellar flux in ergs s-1 cm-2 cm-1.
+     Kurucz stellar flux in ergs s-1 cm-2 cm.
   starwn: 1D ndarray
      Array with wavenumber values in cm^-1.
   tmodel: Scalar
@@ -165,14 +165,16 @@ def readkurucz(kfile, temperature, logg):
   # Get the stellar flux:
   starfl = inten[imodel]  # W m^-2 sr^-1 Hz^-1
 
-  # Convert F_freq to F_lambda  (Hz-1 --> m-1):
-  #   multiply by c/lambda**2 = c*wnumber**2
+  # Convert F_freq to F_wavenumber (Hz-1 --> m):
+  #   multiply by c.
   # Convert units MKS to cgs:
   #   W m-2 = 1e3 ergs s-1 cm-2
   # Convert intensity (astrophysical flux) to flux:
   #   sr-1 = pi
-  starfl = starfl * 1e3 * np.pi * (1e2*sc.c) * starwn**2
-  
+
+  # Flux per wavenumber:  ergs s-1 cm-2 cm
+  starfl = starfl * 1e3 * np.pi * (1e2 * sc.c)
+
   return starfl, starwn, tmodel, gmodel
 
 