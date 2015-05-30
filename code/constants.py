2c6032728e063c2a030f6bf72499c709dcd548da
pcubillos <pcubillos@knights.ucf.edu>
2015-05-30 15:02:42 -0400

    Updated the Transit submodule, fixed a couple of glitched from the previous commit.

---
+++ code/constants.py
@@ -0,0 +1,12 @@
+# List of constants used in the BART project:
+
+# Source: http://nssdc.gsfc.nasa.gov/planetary/factsheet/jupiterfact.html
+# Jupiter mass in kg:
+Mjup = 1.8983e+27
+# Jupiter radius in m:
+Rjup = 7.1492e+07 
+
+# http://nssdc.gsfc.nasa.gov/planetary/factsheet/jupiterfact.html
+# Solar radius in m:
+Rsun = 6.96e+08 
+
